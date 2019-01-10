c636c1928c5685a45d7e441f229f6af4bc5f56b5
prapawity <protfc16@gmail.com>
2019-01-10 14:38:45 +0700

    random and UIImage

--- I AM YOUR FATHER/I AM YOUR FATHER/ViewController.swift
+++ I AM YOUR FATHER/I AM YOUR FATHER/ViewController.swift
@@ -11,6 +11,8 @@ import AVFoundation
 var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
+    @IBOutlet weak var pic: UIImageView!
+    
     var num1: Int = 0,num2: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +20,17 @@ class ViewController: UIViewController {
     }
     let url = URL(fileURLWithPath: Bundle.main.path(forResource: "test.mp3", ofType:nil, inDirectory: "audio")!)
     @IBAction func button(_ sender: UIButton) {
-        num1 = Int.random(in: 1 ... 6)
+        pic.image = UIImage(named: "pic2") // set img to new pic
+        
+        num1 = Int.random(in: 1 ... 6) // random number
         num2 = Int.random(in: 1 ... 6)
+        print("\(num1) \(num2)")
         do{
             player = try AVAudioPlayer(contentsOf: url)
             player?.play()
         }catch{
             print(error.localizedDescription)
         }
-        print("\(num1)"+" "+"\(num2)")
     }
     
     
--- I AM YOUR FATHER/I AM YOUR FATHER/dataType.playground/Contents.swift
+++ I AM YOUR FATHER/I AM YOUR FATHER/dataType.playground/Contents.swift
@@ -15,5 +15,9 @@ print(name+" \(num)") //ITKMITL 15
 var boolean : Bool = true
 var floating : Float = 1.555
 var doubbles : Double = 1.23231
-
+var arrays = ["text", "123", 1234] as [Any] // arrays
+for j in arrays {
+    print(j)
+}
+print(arrays[0])
 
