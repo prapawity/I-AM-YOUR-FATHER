16b284cd737d71e8a9bcabade654fe6f8d4a7c56
prapawity <protfc16@gmail.com>
2019-01-10 13:07:10 +0700

    add ITCAMP Project and Data Type

---
+++ I AM YOUR FATHER/I AM YOUR FATHER/dataType.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// use camelCase
+// var == can edit
+// let == constant
+
+var num: Int = 9
+num = 10
+print(num) //10
+
+let num2 = 10
+let name: String = "ITKMITL"
+num = 15
+print(name+" \(num)") //ITKMITL 15
+var boolean : Bool = true
+var floating : Float = 1.555
+var doubbles : Double = 1.23231
+
+
